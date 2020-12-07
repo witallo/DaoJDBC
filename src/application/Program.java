641b1042fc91ce68e6b33ac0a21e489c72d9776d
Rick Witallo <ricklp@hotmail.com>
2020-12-07 10:13:32 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -8,6 +11,10 @@ public class Program {
 		// TODO Auto-generated method stub
 		Department obj = new Department (1,"Books");
 		System.out.println(obj.toString());
+		
+		Seller seller = new Seller(21,"Bob","bob@gmail.com",new Date(),100.0,obj);
+		
+		System.out.println(seller.toString());
 	}
 
 }
